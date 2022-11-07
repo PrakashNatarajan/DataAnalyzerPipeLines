0d4fb24db8503847d783bead3ff7ca70e52978fe
Prakash Natarajan <prakashntrjn@gmail.com>
2022-11-08 02:01:26 +0530

    Renamed dag variable

--- data_analyst_test_1.py
+++ data_analyst_test_1.py
@@ -17,21 +17,21 @@ def _validate_source_file():
     return 'drop_destination_data'
   return 'validation_failed_exception'
   
-dag = DAG(dag_id='data_analyst_test_1', schedule='@daily', default_args=default_args, catchup=False)
+etl_dag = DAG(dag_id='data_analyst_test_1', schedule='@daily', default_args=default_args, catchup=False)
 
-build_file_paths = EmptyOperator(task_id='build_file_paths', dag=dag)
+build_file_paths = EmptyOperator(task_id='build_file_paths', dag=etl_dag)
 
-check_source_status = BranchPythonOperator(task_id='check_source_status', python_callable=_check_source_file_status, do_xcom_push=False, dag=dag)
-source_file_exception = EmptyOperator(task_id='source_file_exception', dag=dag)
+check_source_status = BranchPythonOperator(task_id='check_source_status', python_callable=_check_source_file_status, do_xcom_push=False, dag=etl_dag)
+source_file_exception = EmptyOperator(task_id='source_file_exception', dag=etl_dag)
 
-download_source_file = EmptyOperator(task_id='download_source_file', dag=dag)
+download_source_file = EmptyOperator(task_id='download_source_file', dag=etl_dag)
 
-validate_source_file = BranchPythonOperator(task_id='validate_source_file', python_callable=_validate_source_file, do_xcom_push=False, dag=dag)
-validation_failed_exception = EmptyOperator(task_id='validation_failed_exception', dag=dag)
+validate_source_file = BranchPythonOperator(task_id='validate_source_file', python_callable=_validate_source_file, do_xcom_push=False, dag=etl_dag)
+validation_failed_exception = EmptyOperator(task_id='validation_failed_exception', dag=etl_dag)
 
-drop_destination_data = EmptyOperator(task_id='drop_destination_data', trigger_rule='none_failed_or_skipped', dag=dag)
+drop_destination_data = EmptyOperator(task_id='drop_destination_data', trigger_rule='none_failed_or_skipped', dag=etl_dag)
 
-transform_load_destination = EmptyOperator(task_id='transform_load_destination', trigger_rule='none_failed_or_skipped', dag=dag)
+transform_load_destination = EmptyOperator(task_id='transform_load_destination', trigger_rule='none_failed_or_skipped', dag=etl_dag)
 
 
 build_file_paths >> check_source_status >> [download_source_file, source_file_exception]
