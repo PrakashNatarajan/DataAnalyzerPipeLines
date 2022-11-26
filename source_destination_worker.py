654559929ca7ff85f5d1623522d02dd6a4ad8837
Prakash Natarajan <prakashntrjn@gmail.com>
2022-11-26 22:49:28 +0530

    Addition and Changes for Correction

--- source_destination_worker.py
+++ source_destination_worker.py
@@ -3,11 +3,15 @@ from datetime import datetime
 def build_source_file_path(configs):
   src_file_path = "{aws_full_dir}/{aws_file_name}_{timestamp}.csv000"
   current_datetime = datetime.now()
+  aws_dir_paths = ""
   pre_dir_cnt = configs['AWS_FOLDER'].count("../")
-  aws_dir_paths = configs['AWS_DIR'].split("/")
-  aws_dir_paths = "/".join(aws_dir_paths[:-(pre_dir_cnt)])
-  aws_folders = configs['AWS_FOLDER'].replace("..","")
-  aws_dir_paths += aws_folders
+  if pre_dir_cnt > 0:
+    aws_dir_paths = configs['AWS_DIR'].split("/")
+    aws_dir_paths = "/".join(aws_dir_paths[:-(pre_dir_cnt)])
+    aws_folders = configs['AWS_FOLDER'].replace("..","")
+    aws_dir_paths += aws_folders
+  else:
+    aws_dir_paths = configs['AWS_DIR'] + "/" + configs['AWS_FOLDER']
   curr_timestamp = current_datetime.strftime("%Y%m%d")
   src_file_path = src_file_path.format(aws_full_dir = aws_dir_paths, aws_file_name = configs['AWS_FILE'], timestamp = curr_timestamp)
   return src_file_path
