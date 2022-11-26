74416f232e5e44e0ec27f05ca4bd15150b28a88e
Prakash Natarajan <prakashntrjn@gmail.com>
2022-11-26 17:55:14 +0530

    Changes

--- data_analyst_test_3.py
+++ data_analyst_test_3.py
@@ -9,12 +9,12 @@ import source_destination_worker as src_dst_worker
 default_args = {'start_date': datetime(2022, 11, 7)}
 loader_configs = {}
 
-def _build_file_paths(loader_configs):
+def _build_file_paths():
   loader_configs['src_file_path'] = src_dst_worker.build_source_file_path(configs=loader_configs)
   loader_configs['dst_file_path'] = src_dst_worker.build_destination_file_path(configs=loader_configs)
   #return context
 
-def _check_source_file_status(loader_configs):
+def _check_source_file_status():
   print("\n\n\n")
   print("Checking Source File Status")
   for key, val in loader_configs.items():
@@ -25,16 +25,14 @@ def _check_source_file_status(loader_configs):
     return 'download_source_file'
   return 'source_file_exception'
 
-def _source_file_exception(loader_configs):
+def _source_file_exception():
   print("\n\n\n")
   print("Sent Mail")
-  print(loader_configs)
   print("\n\n\n")
 
-def _download_source_file(loader_configs):
+def _download_source_file():
   print("\n\n\n")
   print("Downloaded File")
-  print(loader_configs)
   print("\n\n\n")
 
 def _validate_source_file():
@@ -43,40 +41,37 @@ def _validate_source_file():
     return 'drop_destination_data'
   return 'validation_failed_exception'
 
-def _validation_failed_exception(loader_configs):
+def _validation_failed_exception():
   print("\n\n\n")
   print("Sent Mail")
-  print(loader_configs)
   print("\n\n\n")
 
-def _drop_destination_data(loader_configs):
+def _drop_destination_data():
   print("\n\n\n")
   print("Dropped Existing Data")
-  print(loader_configs)
   print("\n\n\n")
 
-def _transform_load_destination(loader_configs):
+def _transform_load_destination():
   print("\n\n\n")
   print("Transform and Loading New Data")
-  print(loader_configs)
   print("\n\n\n")
   
 dat_dag = DAG(dag_id='data_analyst_test_3', schedule='@daily', default_args=default_args, catchup=False)
 
 loader_configs = configs_worker.fetch_loader_configs(loader_name="QGP_USER_LEVEL")
-build_file_paths = PythonOperator(task_id='build_file_paths', python_callable=_build_file_paths, op_kwargs = {"loader_configs" : loader_configs}, dag=dat_dag)
+build_file_paths = PythonOperator(task_id='build_file_paths', python_callable=_build_file_paths, dag=dat_dag)
 
-check_source_status = BranchPythonOperator(task_id='check_source_status', python_callable=_check_source_file_status, op_kwargs = {"loader_configs" : loader_configs}, do_xcom_push=False, dag=dat_dag)
-source_file_exception = PythonOperator(task_id='source_file_exception', python_callable=_source_file_exception, op_kwargs = {"loader_configs" : loader_configs}, dag=dat_dag)
+check_source_status = BranchPythonOperator(task_id='check_source_status', python_callable=_check_source_file_status, do_xcom_push=False, dag=dat_dag)
+source_file_exception = PythonOperator(task_id='source_file_exception', python_callable=_source_file_exception, dag=dat_dag)
 
-download_source_file = PythonOperator(task_id='download_source_file', python_callable=_download_source_file, op_kwargs = {"loader_configs" : loader_configs}, dag=dat_dag)
+download_source_file = PythonOperator(task_id='download_source_file', python_callable=_download_source_file, dag=dat_dag)
 
 validate_source_file = BranchPythonOperator(task_id='validate_source_file', python_callable=_validate_source_file, do_xcom_push=False, dag=dat_dag)
-validation_failed_exception = PythonOperator(task_id='validation_failed_exception', python_callable=_validation_failed_exception, op_kwargs = {"loader_configs" : loader_configs}, dag=dat_dag)
+validation_failed_exception = PythonOperator(task_id='validation_failed_exception', python_callable=_validation_failed_exception, dag=dat_dag)
 
-drop_destination_data = PythonOperator(task_id='drop_destination_data', trigger_rule='none_failed_or_skipped', python_callable=_drop_destination_data, op_kwargs = {"loader_configs" : loader_configs}, dag=dat_dag)
+drop_destination_data = PythonOperator(task_id='drop_destination_data', trigger_rule='none_failed_or_skipped', python_callable=_drop_destination_data, dag=dat_dag)
 
-transform_load_destination = PythonOperator(task_id='transform_load_destination', trigger_rule='none_failed_or_skipped', python_callable=_transform_load_destination, op_kwargs = {"loader_configs" : loader_configs}, dag=dat_dag)
+transform_load_destination = PythonOperator(task_id='transform_load_destination', trigger_rule='none_failed_or_skipped', python_callable=_transform_load_destination, dag=dat_dag)
 
 
 build_file_paths >> check_source_status >> [download_source_file, source_file_exception]
