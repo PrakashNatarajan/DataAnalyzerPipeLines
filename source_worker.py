4f02d42c759cc4a2d0d3662d26b75cf628e0919c
Prakash Natarajan <prakashntrjn@gmail.com>
2022-11-29 00:35:23 +0530

    Changes

--- source_worker.py
+++ source_worker.py
@@ -2,6 +2,7 @@ import boto3
 import botocore
 import configs_worker
 
+
 def check_file_status(loader):
   src_file_status = False
   try:
@@ -18,14 +19,14 @@ def download_source_file(loader):
   object.download_file(loader['dst_file_path'])
 
 
+def _build_sss_object(src_file_path):
+  bucket = _build_sss_bucket()
+  object = bucket.Object(src_file_path)
+  return object 
+
+
 def _build_sss_bucket():
   configs = configs_worker.fetch_aws_configs('AWS_SSS')
   resource = boto3.resource('s3', aws_access_key_id = configs['ACCESS_KEY'], aws_secret_access_key = configs['SECRET_KEY'], region_name = configs['REGION'])
   bucket = resource.Bucket(configs['BUCKET'])
   return bucket 
-
-
-def _build_sss_object(src_file_path):
-  bucket = _build_sss_bucket()
-  object = bucket.Object(src_file_path)
-  return object 
