b37e6074ce0507fa0f4912fbd47605a4b9dedbb1
Prakash Natarajan <prakashntrjn@gmail.com>
2022-11-29 21:12:48 +0530

    Addition of Validation worker

---
+++ validation_worker.py
@@ -0,0 +1,26 @@
+from csv import DictReader
+
+
+def right_column_names?(loader):
+  valid_columns = False
+  column_names = _fetch_header_columns(loader['src_file_path'])
+  equal_columns = column_names == loader['MONGO_FIELDS']
+  mapped_columns = zip(tuple(column_names), tuple(loader['MONGO_FIELDS']))
+  mapped_columns = list(mapped_columns)
+  matched_columns = all([zip_colm[0] == zip_colm[1] for zip_colm in mapped_columns])
+  if equal_columns == matched_columns:
+    valid_columns = True
+  return valid_columns
+
+
+def _fetch_header_columns(file_path):
+  # open file in read mode
+  column_names = []
+  with open(file_path, mode = 'r', encoding = 'ISO8859-1') as read_obj:  # with encoding
+  #with open(file_path, mode = 'r', encoding = 'utf-8') as read_obj:  # with encoding
+    # pass the file object to DictReader() to get the DictReader object
+    csv_dict_reader = DictReader(read_obj, delimiter='|')  # With delimiter
+    # get column names from a csv file
+    column_names = csv_dict_reader.fieldnames
+  return column_names
+
