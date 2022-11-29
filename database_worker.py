276c9c28195d0d01577d8d9da16cb9b108d2a605
Prakash Natarajan <prakashntrjn@gmail.com>
2022-11-29 22:16:22 +0530

    Addition of Database worker

---
+++ database_worker.py
@@ -0,0 +1,25 @@
+import configs_worker
+import mysql.connector as mysql_client
+
+
+def fetch_record_query(loader):
+  cursor = _conn_client_cursor()
+  result = cursor.fetchall()
+
+def create_record_query(table, columns, values):
+  cursor = _conn_client_cursor()
+
+
+def update_record_query(table, columns, values):
+  cursor = _conn_client_cursor()
+
+
+def delete_record_query(table, columns, values):
+  cursor = _conn_client_cursor()
+
+
+def _conn_client_cursor():
+  configs = configs_worker.fetch_db_configs(db_name="ICUBEDB")
+  connection = mysql_client.connect(host=configs['HOST'], user=configs['USERNAME'], password=configs['PASSWORD'], database=configs['DATABASE'], auth_plugin='mysql_native_password', port=configs['PORT'])
+  cursor = connection.cursor()
+  return cursor 
