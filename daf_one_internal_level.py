dc45d7b94519f003b1b284b66801c2e12dfdd9da
Prakash Natarajan <prakashntrjn@gmail.com>
2022-12-01 00:28:38 +0530

    Addition and Changes

--- daf_one_internal_level.py
+++ daf_one_internal_level.py
@@ -15,7 +15,7 @@ default_args = {'start_date': datetime(2022, 11, 7)}
 loader_configs = {}
 
 def _build_file_paths():
-  loader_configs['src_file_path'] = file_paths_worker.build_source_file_path(configs=loader_configs)
+  loader_configs['src_file_path'] = file_paths_worker.build_source_time_file_path(configs=loader_configs)
   loader_configs['dst_file_path'] = file_paths_worker.build_destination_file_path(configs=loader_configs)
   loader_configs['current_time'] = datetime.now().strftime("%Y%m%d%H%M")
   #tsk_intx.xcom_push(key='loader_configs', value=loader_configs)
--- file_paths_worker.py
+++ file_paths_worker.py
@@ -16,6 +16,7 @@ def build_source_file_path(configs):
   src_file_path = src_file_path.format(aws_full_dir = aws_dir_paths, aws_file_name = configs['AWS_FILE'], timestamp = curr_timestamp)
   return src_file_path
 
+
 def build_destination_file_path(configs):
   dst_file_path = "{dst_full_dir}/{dst_file_name}_{curr_timestamp}.csv"
   dst_full_dir = configs['IMPORT_DIR'] + "/" + configs['LOCAL_FOLDER'].split("/")[0]
@@ -23,3 +24,19 @@ def build_destination_file_path(configs):
   dst_file_path = dst_file_path.format(dst_full_dir = dst_full_dir, dst_file_name = configs['AWS_FILE'], curr_timestamp = curr_timestamp)
   return dst_file_path
 
+
+def build_source_time_file_path(configs):
+  src_file_path = "{aws_full_dir}/{aws_file_name}_{timestamp}.csv000"
+  current_datetime = datetime.now()
+  aws_dir_paths = ""
+  pre_dir_cnt = configs['AWS_FOLDER'].count("../")
+  if pre_dir_cnt > 0:
+    aws_dir_paths = configs['AWS_DIR'].split("/")
+    aws_dir_paths = "/".join(aws_dir_paths[:-(pre_dir_cnt)])
+    aws_folders = configs['AWS_FOLDER'].replace("..","")
+    aws_dir_paths += aws_folders
+  else:
+    aws_dir_paths = configs['AWS_DIR'] + "/" + configs['AWS_FOLDER']
+  curr_timestamp = current_datetime.strftime("%Y%m%d%H%M00")
+  src_file_path = src_file_path.format(aws_full_dir = aws_dir_paths, aws_file_name = configs['AWS_FILE'], timestamp = curr_timestamp)
+  return src_file_path
