b785cf9deac7c51717671a407acb5e857dd1b7e1
Prakash Natarajan <prakashntrjn@gmail.com>
2022-12-02 21:49:38 +0530

    Additions

--- database_worker.py
+++ database_worker.py
@@ -18,6 +18,10 @@ def delete_record_query(table, columns, values):
   cursor = _conn_client_cursor()
 
 
+def get_db_client_cursor():
+  cursor = _conn_client_cursor()
+  return cursor
+
 def _conn_client_cursor():
   configs = configs_worker.fetch_db_configs(db_name="ICUBEDB")
   connection = mysql_client.connect(host=configs['HOST'], user=configs['USERNAME'], password=configs['PASSWORD'], database=configs['DATABASE'], auth_plugin='mysql_native_password', port=configs['PORT'])
--- trans_load_worker.py
+++ trans_load_worker.py
@@ -1,5 +1,5 @@
 import csv
-
+import database_worker
 
 def user_auth_master(loader):
   with open(loader['dst_file_path'], 'r') as read_obj:
@@ -38,12 +38,15 @@ def external_role_hierarchy(loader):
 
 
 def daf_internal_level(loader):
+  cursor = database_worker.get_db_client_cursor()
+  placeholders = ', '.join(['%s'] * len(loader['COLUMNS']))
+  insert_query = "INSERT INTO %s ( %s ) VALUES ( %s )" % (loader['TABLE'], loader['COLUMNS'], placeholders)
   with open(loader['dst_file_path'], 'r') as read_obj:
     #csv_dict_reader = csv.DictReader(read_obj) # pass the file object to DictReader() to get the DictReader object
     csv_dict_reader = csv.DictReader(read_obj, delimiter='|')  # With delimiter
     # iterate over each line as a ordered dictionary
     for row in csv_dict_reader:
-      print(row)
+      cursor.execute(insert_query, mydict.values())
 
 
 def daf_external_level(loader):
