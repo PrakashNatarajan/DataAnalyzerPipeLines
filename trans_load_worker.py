92a4d219069368de5e841f6f34d05367e07413c1
Prakash Natarajan <prakashntrjn@gmail.com>
2022-12-03 00:05:30 +0530

    Additions

--- trans_load_worker.py
+++ trans_load_worker.py
@@ -39,14 +39,15 @@ def external_role_hierarchy(loader):
 
 def daf_internal_level(loader):
   cursor = database_worker.get_db_client_cursor()
+  columns = ", ".join(loader_configs['COLUMNS'])
   placeholders = ', '.join(['%s'] * len(loader['COLUMNS']))
-  insert_query = "INSERT INTO %s ( %s ) VALUES ( %s )" % (loader['TABLE'], loader['COLUMNS'], placeholders)
+  insert_query = "INSERT INTO %s ( %s ) VALUES ( %s )" % (loader['TABLE'], columns, placeholders)
   with open(loader['dst_file_path'], 'r') as read_obj:
     #csv_dict_reader = csv.DictReader(read_obj) # pass the file object to DictReader() to get the DictReader object
     csv_dict_reader = csv.DictReader(read_obj, delimiter='|')  # With delimiter
     # iterate over each line as a ordered dictionary
     for row in csv_dict_reader:
-      cursor.execute(insert_query, mydict.values())
+      cursor.execute(insert_query, tuple(row.values()))
 
 
 def daf_external_level(loader):
