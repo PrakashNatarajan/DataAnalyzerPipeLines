cad2b8ae0f37ec879c98304d76a32391722842c3
Prakash Natarajan <prakashntrjn@gmail.com>
2022-12-03 12:14:11 +0530

    Additions and Changes

--- trans_load_worker.py
+++ trans_load_worker.py
@@ -39,15 +39,18 @@ def external_role_hierarchy(loader):
 
 def daf_internal_level(loader):
   cursor = database_worker.get_db_client_cursor()
-  columns = ", ".join(loader_configs['COLUMNS'])
+  columns = ", ".join(loader['COLUMNS'])
   placeholders = ', '.join(['%s'] * len(loader['COLUMNS']))
-  insert_query = "INSERT INTO %s ( %s ) VALUES ( %s )" % (loader['TABLE'], columns, placeholders)
+  #insert_format = "INSERT INTO %s ( %s ) VALUES ( %s )" % (loader['TABLE'], columns, placeholders)
+  insert_format = "INSERT INTO %s ( %s ) VALUES {values}" % (loader['TABLE'], columns)
   with open(loader['dst_file_path'], 'r') as read_obj:
     #csv_dict_reader = csv.DictReader(read_obj) # pass the file object to DictReader() to get the DictReader object
     csv_dict_reader = csv.DictReader(read_obj, delimiter='|')  # With delimiter
     # iterate over each line as a ordered dictionary
     for row in csv_dict_reader:
-      cursor.execute(insert_query, tuple(row.values()))
+      #cursor.execute(insert_format, tuple(row.values()))
+      insert_query = insert_format.format(values = str(tuple(row_dict.values())))
+      cursor.execute(insert_query)
 
 
 def daf_external_level(loader):
