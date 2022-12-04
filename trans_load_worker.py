b0f9c93283e3b9dbcd3504067f8f6be7d23c4659
Prakash Natarajan <prakashntrjn@gmail.com>
2022-12-04 12:38:49 +0530

    Addition and Changes

--- trans_load_worker.py
+++ trans_load_worker.py
@@ -57,27 +57,31 @@ def daf_internal_level(loader):
 
 def daf_external_level(loader):
   client, cursor = database_worker.get_db_client_cursor()
+  loader['COLUMNS'][0] = "role_level"
+  loader['COLUMNS'][1] = "role_user"
+  loader['COLUMNS'][4] = "role_plg"
+  loader['COLUMNS'].remove("smn_code")
+  loader['COLUMNS'].remove("smn_name")
   columns = ", ".join(loader['COLUMNS'])
   placeholders = ', '.join(['%s'] * len(loader['COLUMNS']))
-  #insert_format = "INSERT INTO %s ( %s ) VALUES ( %s )" % (loader['TABLE'], columns, placeholders)
-  insert_format = "INSERT INTO %s ( %s ) VALUES {values}" % (loader['TABLE'], columns)
+  insert_format = "INSERT INTO %s ( %s ) VALUES ( %s )" % (loader['TABLE'], columns, placeholders)
   with open(loader['dst_file_path'], 'r') as read_obj:
     #csv_dict_reader = csv.DictReader(read_obj) # pass the file object to DictReader() to get the DictReader object
     csv_dict_reader = csv.DictReader(read_obj, delimiter='|')  # With delimiter
     # iterate over each line as a ordered dictionary
     for row_dict in csv_dict_reader:
-      row_dict['role_level'] = row_dict['role']
+      record_dict = {}
+      record_dict['role_level'] = row_dict['role']
       if row_dict['role'] == "RS":
-        row_dict['role_user'] = row_dict['rscode']
+        record_dict['role_user'] = row_dict['rscode']
       else:
-        row_dict['role_user'] = "_".join([row_dict['rscode'], row_dict['smn_code']])
-      row_dict['role_plg'] = row_dict['plg']
+        record_dict['role_user'] = "_".join([row_dict['rscode'], row_dict['smn_code']])
+      record_dict['role_plg'] = row_dict['plg']
       del row_dict['role']
       del row_dict['rscode']
       del row_dict['smn_code']
       del row_dict['smn_name']
       del row_dict['plg']
-      #cursor.execute(insert_format, tuple(row.values()))
-      insert_query = insert_format.format(values = str(tuple(row_dict.values())))
-      cursor.execute(insert_query)
+      record_dict.update(row_dict)
+      cursor.execute(insert_format, tuple(record_dict.values()))
       client.commit()
